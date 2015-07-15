bf4a4de8fd349919a68771ce9ed145a17ed0b53b
The-WUUSTER <edma_wu@yahoo.com>
2015-07-14 23:23:58 -0400

    Rolling Ball Game
    Made with Unity 5

---
+++ Assets/scripts/Controller.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	public float speed;
+	private int count;
+	private Rigidbody rb;
+	public Text scoreText;
+	public Text victoryText;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		setScoreText ();
+		victoryText.text = "";
+	}
+
+	void FixedUpdate() {
+		float move_horizontal = Input.GetAxis ("Horizontal");
+		float move_vertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (move_horizontal, 0.0f, move_vertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Coin")) {
+			other.gameObject.SetActive (false);
+			count++;
+			setScoreText();
+		}
+	}
+
+	void setScoreText() {
+		scoreText.text = "Score: " + count.ToString ();
+		if (count >= 14) {
+			victoryText.text = "DANKEST VICTORY!";
+		}
+	}
+}
